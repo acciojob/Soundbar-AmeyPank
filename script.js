0e4b413e76ddff971ed51eea50b971e8c477a478
AmeyPank <pankantiameya@.com>
2023-04-13 16:28:48 +0530

    soundbar

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-//your JS code here. If required.
-var audio = null;
+// //your JS code here. If required.
+// var audio = null;
 
-		function playSound(soundFile) {
-			if(audio !== null) {
-				audio.pause();
-				audio.currentTime = 0;
-			}
+// 		function playSound(soundFile) {
+// 			if(audio !== null) {
+// 				audio.pause();
+// 				audio.currentTime = 0;
+// 			}
 
-			audio = new Audio('sounds/' + soundFile);
+// 			audio = new Audio('sounds/' + soundFile);
+// 			audio.play();
+// 		}
+
+// 		function stopSound() {
+// 			if(audio !== null) {
+// 				audio.pause();
+// 				audio.currentTime = 0;
+// 				audio = null;
+// 			}
+// 		}
+
+
+		function playSound() {
+			var audio = document.getElementById("audio");
 			audio.play();
-		}
-
-		function stopSound() {
-			if(audio !== null) {
-				audio.pause();
-				audio.currentTime = 0;
-				audio = null;
-			}
+		  }
+		function stopSound(){
+			var audio = document.getElementById("audio");
+			audio.pause();
 		}
